276aac99a7ea8fda87046460faa1a17c3da5f994
Tom Zhang <tommyawesome08@gmail.com>
2024-08-11 18:27:30 -0400

    hash code is able to be printed

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,13 @@
+import java.util.Arrays;
+
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        System.out.print("Hello and welcome!");
+        String[] list1 = {"a", "b", "c"};
+        String[] list2 = {"a", "b", "c"};
+
+        System.out.println(Arrays.hashCode(list1));
+        System.out.println(Arrays.hashCode(list2));
     }
 }
